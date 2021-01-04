4e88f7091f390bd0829f402b910f46d920fbdf4e
dparhonin <deps@yandex.ru>
2021-01-04 16:50:48 +0300

    Updated.

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
   },
 
   https: {
-    privateKey: '/path/to/privkey.pem',
-    certificate: '/path/to/fullchain.pem',
+    privateKey: '...',
+    certificate: '...',
     port: 8443,
   },
 
@@ -19,8 +19,8 @@ module.exports = {
     {
       id: '1',
       name: 'Yandex',
-      clientId: 'yandex-client-id',
-      clientSecret: 'client-secret',
+      clientId: '...',
+      clientSecret: '...',
       isTrusted: false,
     },
   ],
@@ -28,8 +28,8 @@ module.exports = {
   users: [
     {
       id: '1',
-      username: 'admin',
-      password: 'admin',
+      username: '...',
+      password: '...',
       name: 'Administrator',
     },
   ],
@@ -89,73 +89,73 @@ module.exports = {
         },
       ],
     },
-      {
-          name: 'Свет',
-          room: 'Спальня',
-          type: 'devices.types.light',
-          capabilities: [
-              {
-                  type: 'devices.capabilities.on_off',
-                  retrievable: true,
-                  parameters: {
-                      split: false
-                  },
-                  state: {
-                      instance: 'on',
-                      value: false
-                  }
-              },
-              {
-                  type: 'devices.capabilities.range',
-                  retrievable: true,
-                  parameters: {
-                      instance: 'brightness',
-                      random_access: true,
-                      range: {
-                          min: 0,
-                          max: 100
-                      },
-                      unit: 'unit.percent'
-                  },
-                  state: {
-                      instance: 'brightness',
-                      value: 100
-                  }
-              },
-              {
-                  type: 'devices.capabilities.color_setting',
-                  retrievable: true,
-                  parameters: {
-                      temperature_k: {
-                          min: 2700,
-                          max: 6500
-                      }
-                  },
-                  state: {
-                      instance: 'temperature_k',
-                      value: 4500
-                  }
-              }
-          ],
-          complexState: {
-              publish: 'yandex/devices/bedroomCeilingLamp/set',
-              query: 'yandex/devices/bedroomCeilingLamp/get'
-          }
+    {
+      name: 'Свет',
+      room: 'Спальня',
+      type: 'devices.types.light',
+      capabilities: [
+        {
+          type: 'devices.capabilities.on_off',
+          retrievable: true,
+          parameters: {
+            split: false,
+          },
+          state: {
+            instance: 'on',
+            value: false,
+          },
+        },
+        {
+          type: 'devices.capabilities.range',
+          retrievable: true,
+          parameters: {
+            instance: 'brightness',
+            random_access: true,
+            range: {
+              min: 0,
+              max: 100,
+            },
+            unit: 'unit.percent',
+          },
+          state: {
+            instance: 'brightness',
+            value: 100,
+          },
+        },
+        {
+          type: 'devices.capabilities.color_setting',
+          retrievable: true,
+          parameters: {
+            temperature_k: {
+              min: 2700,
+              max: 6500,
+            },
+          },
+          state: {
+            instance: 'temperature_k',
+            value: 4500,
+          },
+        },
+      ],
+      complexState: {
+        publish: 'yandex/devices/bedroomCeilingLamp/set',
+        query: 'yandex/devices/bedroomCeilingLamp/get',
       },
-      {
-          name: 'Телевизор',
-          room: 'Спальня',
-          type: 'devices.types.media_device.tv',
-          capabilities: [
-              {
-                  type: 'devices.capabilities.on_off',
-                  retrievable: false,
-                  state: {
-                      instance: 'on',
-                      publish: 'yandex/devices/tvset/set',
-                  },
-              },
-          ],
-      },
+    },
+    {
+      name: 'Телевизор',
+      room: 'Спальня',
+      type: 'devices.types.media_device.tv',
+      capabilities: [
+        {
+          type: 'devices.capabilities.on_off',
+          retrievable: false,
+          state: {
+            instance: 'on',
+            publish: 'yandex/devices/tvset/set',
+          },
+        },
+      ],
+    },
   ],
 };
